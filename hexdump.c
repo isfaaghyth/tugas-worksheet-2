2a006afce0d18ba8216e0f4dca7237c694414128
Muh.Isfahani Ghiyath.YM <isfaaghyth@gmail.com>
2017-03-21 12:06:14 +0700

    initial

---
+++ hexdump.c
@@ -0,0 +1,26 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+	unsigned char buffer[16];
+	size_t offset = 0;
+	size_t bytes_read;
+	int i;
+
+	int fd = open(argv[0], O_RDONLY);
+
+	do {
+		bytes_read = read(fd, buffer, sizeof(buffer));
+		for (int i = 0; i < bytes_read; ++i) {
+			printf("%02x", buffer[i]);
+		}
+		printf("\n");
+		offset += bytes_read; 
+	}
+	while (bytes_read == sizeof(buffer));
+	close(fd);
+	return 0;
+}
---
+++ lseek_huge.c
@@ -0,0 +1,18 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]) {
+	int zero = 0;
+	const int megabyte = 1024 * 1024;
+
+	char* filename = argv[1];
+	size_t length = (size_t) atoi(argv[2]) * megabyte;
+
+	int fd = open(filename, O_WRONLY | O_CREAT | O_EXCL, 0666);
+
+	lseek(fd, length - 1, SEEK_SET);
+	write(fd, &zero, 1);
+}
